4eb30051aa59ad5712641177bad71292669dddb5
Vivien-Shah <hfsthane7787@gmail.com>
2025-09-22 19:55:46 +0530

    Add files via upload

---
+++ c19_Trignometry_activity.py
@@ -0,0 +1,10 @@
+'''To find the mean,square root,power,ceiling and floor of the given numbers
+
+import math
+print(math.pow(18,2))
+
+print(math.sqrt(625))
+
+print(math.ceil(7.4))
+
+print(math.floor(6.7))'''
